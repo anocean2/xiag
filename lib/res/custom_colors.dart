d4f0c2e01225e8fbde6901580b66407589f6658b
Andrey <kupriev@gmail.com>
2021-08-09 13:00:23 +0300

    search color

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -2,5 +2,5 @@ import 'dart:ui';
 
 class CustomColors {
   static const accentColor = Color(0xFF42A5F5);
-  static const searchBackgroundColor = Color(0xFFF9F9F9);
+  static const searchBackgroundColor = Color(0xFFE8E8E8);
 }
